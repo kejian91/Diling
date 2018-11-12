3c05321411083443266d592e247084a2ad0f57a7
Leon Jin <yanqing.jin@philips.com>
2018-11-12 20:29:01 +0800

    code change

--- lanbin/src/main/java/com/diling/controller/PageController.java
+++ lanbin/src/main/java/com/diling/controller/PageController.java
@@ -7,7 +7,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class PageController {
 
     @GetMapping(value = "/")
+    public String indexPage() {
+        return "home";
+    }
+
+    @GetMapping(value = "/home")
     public String homePage() {
         return "home";
+    }
+
+    @GetMapping(value = "/measure")
+    public String measurePage() {
+        return "measure";
     }
 }
